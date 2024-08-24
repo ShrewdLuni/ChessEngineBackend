bec1ad560badbb251bc7ecc308e03913374727dd
ShrewdLuni <87246480+ShrewdLuni@users.noreply.github.com>
2024-08-24 13:38:04 +0300

    feat: update recevice funciton logic

--- ChessApp/consumers.py
+++ ChessApp/consumers.py
@@ -2,6 +2,10 @@ import json
 import random
 from channels.generic.websocket import WebsocketConsumer
 
+from ChessEngine.board import Board
+from ChessEngine.moveGenerator import MoveGenerator
+from ChessEngine.precomputedMoveData import PrecomputedMoveData
+
 class MyConsumer(WebsocketConsumer):
     def connect(self):
         self.accept()
@@ -10,8 +14,13 @@ class MyConsumer(WebsocketConsumer):
         pass
 
     def receive(self, text_data):
-        random_int = random.randint(1, 100)
-        
-        self.send(text_data=json.dumps({
-            'random_int': random_int
-        }))
+        print(1)
+        brd = Board()
+        pmd = PrecomputedMoveData()
+        mg = MoveGenerator(brd, pmd)
+        brd.FENtoBoard("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1")
+
+        mg.GenerateLegalMoves()
+        moves_as_dicts = [move.__dict__ for move in mg.moves]
+        moves_json = json.dumps(moves_as_dicts)
+        self.send(text_data=moves_json)
