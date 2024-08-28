b52f492a7efe4ab5a50b7fe3cdd21b350a8d9a7c
ShrewdLuni <87246480+ShrewdLuni@users.noreply.github.com>
2024-08-28 18:45:52 +0300

    feat: add base for new action

--- ChessApp/consumers.py
+++ ChessApp/consumers.py
@@ -5,6 +5,7 @@ from channels.generic.websocket import WebsocketConsumer
 from ChessEngine.board import Board
 from ChessEngine.move_generator import MoveGenerator
 from ChessEngine.precomputed_move_data import PrecomputedMoveData
+from ChessEngine.engine import Engine
 
 class MyConsumer(WebsocketConsumer):
     def connect(self):
@@ -19,7 +20,8 @@ class MyConsumer(WebsocketConsumer):
 
         action_map = {
             'generate_random_number': self.generate_random_number,
-            'get_chess_info': self.get_chess_info
+            'get_chess_info': self.get_chess_info,
+            'engine_make_move': self.engine_make_move
         }
 
         action_func = action_map.get(action)
@@ -35,11 +37,16 @@ class MyConsumer(WebsocketConsumer):
             'random_number': random_number
         }))
 
+    #update board aka make player move
+    #find and make best move aka update board again
+    #search legal moves for client
+    #send server move and legal moves
+
     def get_chess_info(self):
         brd = Board()
         pmd = PrecomputedMoveData()
         mg = MoveGenerator(brd, pmd)
-        brd.fen_to_board("rnbqkbnr/pppppppp/8/8/8/8/8/RNBQKBNR w KQkq - 0 1")
+        brd.fen_to_board("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1")
 
         mg.generate_legal_moves()
 
@@ -49,3 +56,10 @@ class MyConsumer(WebsocketConsumer):
             'moves': moves_as_dicts
         }))
 
+    def engine_make_move(self):
+        engine = Engine()
+        engine_move = engine.get_random_move("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1")
+        self.send(text_data=json.dumps({
+            'action': 'engine_make_move',
+            'engine_move': engine_move.__dict__
+        }))
