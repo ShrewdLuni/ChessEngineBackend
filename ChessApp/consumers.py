ae57335e98ae16085726ffa2c51774bbac4751d0
ShrewdLuni <87246480+ShrewdLuni@users.noreply.github.com>
2024-08-29 17:39:42 +0300

    refactor: chess login into one function

--- ChessApp/consumers.py
+++ ChessApp/consumers.py
@@ -8,6 +8,10 @@ from ChessEngine.precomputed_move_data import PrecomputedMoveData
 from ChessEngine.engine import Engine
 
 class MyConsumer(WebsocketConsumer):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.engine = Engine()
+
     def connect(self):
         self.accept()
 
@@ -19,47 +23,21 @@ class MyConsumer(WebsocketConsumer):
         action = data.get('action')
 
         action_map = {
-            'generate_random_number': self.generate_random_number,
-            'get_chess_info': self.get_chess_info,
-            'engine_make_move': self.engine_make_move
+            'engine_make_move': lambda: self.engine_make_move(data.get('move'))
         }
-
         action_func = action_map.get(action)
         if action_func:
             action_func()
         else:
             self.send(text_data=json.dumps({'error': 'Unknown action'}))
 
-    def generate_random_number(self):
-        random_number = random.randint(1, 100)
-        self.send(text_data=json.dumps({
-            'action': 'generate_random_number',
-            'random_number': random_number
-        }))
-
-    #update board aka make player move
-    #find and make best move aka update board again
-    #search legal moves for client
-    #send server move and legal moves
-
-    def get_chess_info(self):
-        brd = Board()
-        pmd = PrecomputedMoveData()
-        mg = MoveGenerator(brd, pmd)
-        brd.fen_to_board("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1")
-
-        mg.generate_legal_moves()
-
-        moves_as_dicts = [move.__dict__ for move in mg.moves]
-        self.send(text_data=json.dumps({
-            'action': 'get_chess_info',
-            'moves': moves_as_dicts
-        }))
-
-    def engine_make_move(self):
-        engine = Engine()
-        engine_move = engine.get_random_move("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1")
+    def engine_make_move(self, client_move):
+        self.engine.make_move(client_move['starting_square'], client_move['target_square'])
+        engine_move = self.engine.get_random_move()
+        legal_moves = self.engine.get_legal_moves()
+        moves_as_dicts = [move.__dict__ for move in legal_moves]
         self.send(text_data=json.dumps({
             'action': 'engine_make_move',
-            'engine_move': engine_move.__dict__
+            'engine_move': engine_move.__dict__,
+            'moves': moves_as_dicts
         }))
