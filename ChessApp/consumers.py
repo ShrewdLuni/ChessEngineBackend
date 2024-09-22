207947aa9b63b52f3213b280326723c1c1dbe6df
ShrewdLuni <87246480+ShrewdLuni@users.noreply.github.com>
2024-09-22 20:40:29 +0300

    feat: add engine_unmake_move action

--- ChessApp/consumers.py
+++ ChessApp/consumers.py
@@ -19,7 +19,8 @@ class MyConsumer(WebsocketConsumer):
         action = data.get('action')
         action_map = {
             'engine_get_legal_moves': self.engine_get_legal_moves,
-            'engine_make_move': lambda: self.engine_make_move(data.get('move'))
+            'engine_make_move': lambda: self.engine_make_move(data.get('move')),
+            'engine_unmake_move': self.engine_unmake_move
         }
         action_func = action_map.get(action)
         if action_func:
@@ -45,6 +46,13 @@ class MyConsumer(WebsocketConsumer):
     def engine_make_move(self, client_move):
         self.engine.make_move(client_move['starting_square'], client_move['target_square'], client_move['flag'])
         engine_move = self.engine.get_random_move()
+        self.send(text_data=json.dumps({
+            'action': 'engine_make_move',
+            'fen': self.engine.board.fen_from_board(),
+        }))
+
+    def engine_unmake_move(self):
+        self.engine.unmake_move()
         self.send(text_data=json.dumps({
             'action': 'engine_make_move',
             'fen': self.engine.board.fen_from_board(),
